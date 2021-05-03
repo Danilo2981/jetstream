083b4b21904dff6319cb04a8b971e3cbe932724e
vagrant <vagrant@homestead>
2021-05-03 02:57:02 +0000

    Head 1

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tailwind</title>
+
+    {{-- Tailwind CSS --}}
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    @include('tailwind.header')
+</body>
+</html>
---
+++ resources/views/tailwind/header.blade.php
@@ -0,0 +1,19 @@
+<!--Inicio de Cabecera-->
+<header id="header">
+    <div class="wrap">
+        <div id="logo">
+            <span class="gear">S</span>
+            <h3>BLOG</h3>
+        </div>
+
+        <nav id="menu">
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">FORMACIÓN</a></li>
+                <li><a href="#">CONTACTO</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<!--Fin de Cabecera-->
